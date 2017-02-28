7eafe71966c9145461051041b964483b44598000
amberonno <amberonno@gmail.com>
2017-02-28 15:05:10 +0800

    更新less

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,44 @@
 var gulp = require('gulp'),
-    less = require('gulp-less'),
+    webpack = require('webpack'),
     concat = require('gulp-concat'),
     rename = require('gulp-rename'),
+    gutil = require('gulp-util'),
     minifycss = require('gulp-minify-css'),
-    plumber = require('gulp-plumber'),
     clean = require('gulp-clean'),
-    base64 = require('gulp-base64');
+    webpackConfig = require('./webpack.config.js');
 
-
-var cssName = require('./package.json').name,
-    dest = "./dist/css";
+var dest = "./dist/";
 
 var error = function(e){
-  console.error(e);
-  if(e.stack){
-    console.error(e.stack);
-  }
+    console.error(e);
+    if(e.stack){
+        console.error(e.stack);
+    }
 };
 
-var copy = function(source, destination){
-  return gulp.src(source)
-        .pipe(gulp.dest(destination));
-}
-
-var lessFunc = function(){
-  return gulp.src(['./less/'+cssName+'.less'])
-      .pipe(less({ compress: false }))
-      .pipe(plumber())
-      //.on('error', error )
-      .pipe(gulp.dest(dest));
-}
-
 gulp.task('clean', function () {
-    return gulp.src(['./dist/img/*','./dist/css/*'], {read: false})
+    return gulp.src(['./dist/*'], {read: false})
         .pipe(clean()).on('error', error );
 });
-//必须保持img文件和css相对路径的一致性才可以顺利转化base64格式
-gulp.task('copy-image', ['clean'],function(){ // 将字体文件拷贝到dist文件夹下
-    copy('./less/img/*', './dist/img');
+
+gulp.task('webpack', ['clean'], function (done) {
+    webpack(webpackConfig).run(function (err, stats) {
+        if (err) throw new gutil.PluginError("webpack", err);
+        gutil.log("[webpack]", stats.toString({}));
+        done();
+    });
 });
 
-gulp.task('less',['copy-image'], lessFunc);
+gulp.task('min-styles', ['webpack'], function() {
+    gulp.src(['./dist/*.css'])
+        // .pipe(concat(cssName+'.css') // 合并文件为all.css
+        .pipe(gulp.dest(dest)) // 输出all.css文件
+        .pipe(rename({ suffix: '.min' })) // 重命名all.css为 all.min.css
+        .pipe(minifycss()) // 压缩css文件
+        .pipe(gulp.dest(dest)); // 输出all.min.css
+});
 
-gulp.task('min-styles', ['base64'], function() {
-  gulp.src([dest+'/*.css'])
-      // .pipe(concat(cssName+'.css') // 合并文件为all.css
-      .pipe(gulp.dest(dest)) // 输出all.css文件
-      .pipe(rename({ suffix: '.min' })) // 重命名all.css为 all.min.css
-      .pipe(minifycss()) // 压缩css文件
-      .pipe(gulp.dest(dest)); // 输出all.min.css
-});
-//将less编译为css后进行小图片base64格式的转化
-gulp.task('base64', ['less'], function() {
-    return gulp.src([dest+'/*'])
-        .pipe(base64())
-        .pipe(gulp.dest(dest));
-});
 gulp.task('default', ['min-styles']);
+gulp.task('dev',['webpack'], function() {
+    gulp.watch('./less/**/*.less', ['webpack']);
+});
